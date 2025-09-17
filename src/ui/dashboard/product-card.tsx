ff3c200916d71335235f061c5af5b6f828c3b712
Maks Bessonov <max8essonov@gmail.com>
2025-09-17 21:37:50 +0500

    feat: enhance ProductCard component with play button and improve layout structure

--- src/ui/dashboard/product-card.tsx
+++ src/ui/dashboard/product-card.tsx
@@ -1,5 +1,6 @@
 import { type ProductCardType } from "@/lib/types";
 import CardInfo from "@/ui/dashboard/card-info";
+import Image from "next/image";
 import ProductCardBookmarkedIcon from "./icons/ProductCardBookmarkedIcon";
 import clsx from "clsx";
 
@@ -29,9 +30,9 @@ export default function ProductCard({ product, isTrendingProduct }: Props) {
     >
       <div
         className={clsx(
-          `flex flex-col rounded-[.5rem] [background-image:var(--bg-large)] bg-cover bg-no-repeat`,
+          `group grid grid-rows-3 rounded-[.5rem] [background-image:var(--bg-large)] bg-cover bg-no-repeat`,
           {
-            "h-[8.75rem] justify-between px-4 py-[0.875rem] md:h-[14.375rem] md:px-6 md:py-[1.34375rem]":
+            "h-[8.75rem] px-4 py-[0.875rem] md:h-[14.375rem] md:px-6 md:py-[1.34375rem]":
               isTrending,
             "h-[6.875rem] p-2 md:h-[8.75rem] md:p-4 lg:h-[10.875rem]":
               !isTrending,
@@ -39,10 +40,22 @@ export default function ProductCard({ product, isTrendingProduct }: Props) {
         )}
         style={cardStyle as React.CSSProperties}
       >
-        <div className="self-end">
+        <div className="justify-self-end">
           <ProductCardBookmarkedIcon product={product} />
         </div>
-        {isTrending && <CardInfo product={product} isTrending={isTrending} />}
+        <div className="flex min-w-[117px] grow-0 cursor-pointer items-center gap-3 self-center justify-self-center rounded-full bg-black/50 p-[9px] opacity-0 transition-opacity duration-200 group-hover:opacity-100 hover:bg-white/25">
+          <div className="relative flex size-7.5">
+            <Image fill src="/icons/icon-play.svg" alt="play icon" />
+          </div>
+          <p className="text-lg">Play</p>
+        </div>
+        {isTrending ? (
+          <div className="self-end">
+            <CardInfo product={product} isTrending={isTrending} />
+          </div>
+        ) : (
+          <div className="self-end"></div>
+        )}
       </div>
       {!isTrending && <CardInfo product={product} />}
     </div>
